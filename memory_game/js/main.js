79927834271d868fba5b3ece81f257c37d5ece6c
Axel Campero <axelcampero@Axels-MBP.fios-router.home>
2019-11-27 09:14:48 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,40 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay =[];
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You Found a match");
+	}else{
+		console.log("Sorry, try again")
+	}
+}
+function flipCard(cardId){
+	
+
+	
+	//checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+		if(cardsInPlay.length === 2){
+		checkForMatch();
+	/*if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	}
+	else{
+		alert("sorry Try again");*/
+
+
+	}
+	
+
+};
+
+flipCard(0);
+flipCard(2);
+//checkForMatch();
+
+
+
+
+	
+
