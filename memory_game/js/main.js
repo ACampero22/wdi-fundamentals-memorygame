18f9797a63cc993f81f2c147f809ae6e6e316b7a
Axel Campero <axelcampero@Axels-MBP.fios-router.home>
2019-11-28 19:49:40 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,37 +1,54 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
 const cardsInPlay =[];
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		console.log("You Found a match");
 	}else{
-		console.log("Sorry, try again")
+		//console.log("Sorry, try again");
+		alert("Sorry, try again");
 	}
 }
 function flipCard(cardId){
-	
 
-	
-	//checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 
 		if(cardsInPlay.length === 2){
 		checkForMatch();
-	/*if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("you found a match!");
+		
 	}
-	else{
-		alert("sorry Try again");*/
-
-
-	}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	
 
 };
 
 flipCard(0);
 flipCard(2);
-//checkForMatch();
+
+
 
 
 
